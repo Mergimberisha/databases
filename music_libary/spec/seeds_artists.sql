1dcf0fe44acc2a670f7236313ce581947edfc666
Mergim <mergimberisha0@gmail.com>
2023-04-24 17:36:28 +0100

    Created a new database called music_library_test

--- music_libary/spec/seeds_artists.sql
+++ music_libary/spec/seeds_artists.sql
@@ -9,5 +9,5 @@ TRUNCATE TABLE students RESTART IDENTITY; -- replace with your own table name.
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 
-INSERT INTO students (name, cohort_name) VALUES ('David', 'April 2022');
-INSERT INTO students (name, cohort_name) VALUES ('Anna', 'May 2022');
+INSERT INTO artists (name, genre) VALUES('Pixies', 'Rock');
+INSERT INTO artists (name, genre) VALUES('ABBA', 'Pop');
