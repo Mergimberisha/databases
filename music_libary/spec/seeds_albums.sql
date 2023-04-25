beca5e19932cc5e365099380c7fe010aef4557ee
Mergim <mergimberisha0@gmail.com>
2023-04-25 13:56:29 +0100

    added album.rb and album_repository.rb with a class inside aswell as updated albums recipe.md with the seeds fils

--- music_libary/spec/seeds_albums.sql
+++ music_libary/spec/seeds_albums.sql
@@ -4,10 +4,13 @@
 -- so we can start with a fresh state.
 -- (RESTART IDENTITY resets the primary key)
 
-TRUNCATE TABLE students RESTART IDENTITY; -- replace with your own table name.
+TRUNCATE TABLE artists RESTART IDENTITY;
+TRUNCATE TABLE albums RESTART IDENTITY; -- replace with your own table name.
 
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 
-INSERT INTO students (name, cohort_name) VALUES ('David', 'April 2022');
-INSERT INTO students (name, cohort_name) VALUES ('Anna', 'May 2022');
+INSERT INTO artists (name, genre) VALUES ('Pixies', 'Rock');
+
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Bossanova', '1999', '1');
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Surfer Rosa', '2001', '1');
